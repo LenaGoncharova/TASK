94d7e3b1856aa262c248a57a9089fdd566d96418
Елена <egoncharova72@yandex.ru>
2022-11-15 19:54:33 +0400

    5

---
+++ task5/Program.cs
@@ -0,0 +1,10 @@
+﻿// напишите программу, в которой на входе натуральное число N,а на выходе ряд чисел от -N  до N. 
+Console.Clear();
+Console.WriteLine("введите натуральное число - ");
+int N = Convert.ToInt32( Console.ReadLine());
+int A =-N;
+while(A<=N)
+{
+  Console.WriteLine(A);
+  A++;
+}
