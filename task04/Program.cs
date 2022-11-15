8dcec5efd8675968c9e769b1d346e6802e920e23
Елена <egoncharova72@yandex.ru>
2022-11-15 20:31:46 +0400

    04

---
+++ task04/Program.cs
@@ -0,0 +1,17 @@
+﻿// на входе - три числа, на выхое - максимальное из них.
+Console.Clear();
+Console.WriteLine ("Введите число A - ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число B - ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число C - ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+int maximum =  A;
+
+if (maximum <B) maximum = B;
+if (maximum <C) maximum = C;
+{
+    Console.Write ("максимальное число -");
+    Console.Write ( maximum );
+}
