6d823683e9d3e0a035f666bc9ad7282461fd93f1
Елена <egoncharova72@yandex.ru>
2022-11-15 19:54:58 +0400

    7

---
+++ task7/Program.cs
@@ -0,0 +1,6 @@
+﻿// Программа, которая на вход принимае трехзначное число, а на выходе дает последнюю цифру этого числа 
+Console.Clear();
+Console.WriteLine("Введите трехзначное число  - ");
+int count = Convert.ToInt32(Console.ReadLine());
+int F = count%10;
+Console.WriteLine (F);
