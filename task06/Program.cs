13266a12dd4a4d26c161e78562671b78a0689fc9
Елена <egoncharova72@yandex.ru>
2022-11-15 20:55:24 +0400

    06

---
+++ task06/Program.cs
@@ -0,0 +1,13 @@
+﻿// на входе число, на выходе - его четность или нечетность.
+Console.Clear();
+Console.WriteLine(" Введите число - ");
+int A = Convert.ToInt32(Console.ReadLine());
+int res = A%2;
+if(res==0)
+{
+    Console.WriteLine($"{A}- четное число ");
+}
+else
+{
+   Console.WriteLine($"{A}- нечетное число "); 
+}
