289ae77c19567b47c4a5950a1fa032bfd871e3db
Елена <egoncharova72@yandex.ru>
2022-11-15 20:24:22 +0400

    02

---
+++ task02/Program.cs
@@ -0,0 +1,27 @@
+﻿// на вход принимает два числа, на выход выдает какое из них большее и меньшее.
+Console.Clear();
+Console.WriteLine("Введите первое число -  ");
+int A = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите второе  число -  ");
+int B = Convert.ToInt32(Console.ReadLine()); 
+
+
+ int max = 0;
+ int min = 0;
+
+if ( A >  B)
+{
+   max = A;
+   min = B;
+}
+else
+{
+   max = B;
+   min = A;
+}
+
+Console.Write("максимальное число - ");
+Console.WriteLine( max);
+
+Console.Write("минимальное число -");
+Console.WriteLine(min);
