7a3a56a9bc21ce4210b1a249cdd919a6d2dd163a
Елена <egoncharova72@yandex.ru>
2022-11-16 11:54:08 +0400

    08

---
+++ task08/Program.cs
@@ -0,0 +1,20 @@
+﻿//На входе число N, на выходе ряд из четных чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine ("Введите число -  ");
+int N = Convert.ToInt32( Console.ReadLine());
+int A = 1;
+
+ while (A <= N)
+{
+    if ( A%2 ==0) 
+
+    {
+       Console.Write( A  );
+    }
+    else
+    {
+       Console.WriteLine(  );
+    }
+   A++;
+}
