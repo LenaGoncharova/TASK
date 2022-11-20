558fceaa1e37eeb940d7ec1a8fe62300644779a5
Елена <egoncharova72@yandex.ru>
2022-11-20 22:07:08 +0400

    14

---
+++ task14/Program.cs
@@ -0,0 +1,14 @@
+﻿// напишите программу, которая принимает на вход число  и проверяет
+// кратно ли оно одновременно 7ии 23.
+Console.Clear();
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool MultiplicityTwoDigits (int num)
+{
+return num % 7 == 0 && num % 23 == 0;
+}
+
+bool result = MultiplicityTwoDigits(number);
+Console.WriteLine(result ? "Да" : "Нет");
