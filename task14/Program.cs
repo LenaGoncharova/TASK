bfc06911c4d8c4b58548d322d9aad27e590b082d
Елена <egoncharova72@yandex.ru>
2022-11-20 22:15:00 +0400

    14

--- task14/Program.cs
+++ task14/Program.cs
@@ -12,3 +12,16 @@ return num % 7 == 0 && num % 23 == 0;
 
 bool result = MultiplicityTwoDigits(number);
 Console.WriteLine(result ? "Да" : "Нет");
+
+// Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool MultiplicityTwoDigits (int num, int num1, int num2)
+{
+return num % num1 == 0 && num % num2 == 0;
+}
+
+bool result = MultiplicityTwoDigits(number, 7, 23);
+
+Console.Write($"{number} -> ");
+Console.WriteLine(result ? "Да" : "Нет");
