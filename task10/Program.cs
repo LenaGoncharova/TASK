dda20c4fc76da851fad3a4365b2b5e7f5f38fd84
Елена <egoncharova72@yandex.ru>
2022-11-21 20:36:51 +0400

    10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,4 +1,18 @@
-﻿// напишите программу, которая принимает на вход трёхзначное число и на
-using static System.Console;
-int number = new Random(). Next(100, 1000);
-WriteLine( number);
+﻿// Напишите программу, которая принимает на вход трёхзначное число и
+//  на выходе показывает вторую цифру этого числа.
+//  Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+//int number = new Random(). Next(100, 1000);
+// Console.WriteLine( number);
+// int res = ((number/10)%10);
+// Console.WriteLine($"{number}->{res}");
+
+int SecondDigit( int num)
+{
+    int res=(num/10)%10;
+    return res ;
+}
+int number  = new Random(). Next(100, 1000);
+int resalt=SecondDigit(number);
+
+Console.WriteLine($"{number}->{resalt}");
