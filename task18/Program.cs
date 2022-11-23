2633bf3abad5638bd44e43bac80d02d7c3ca3526
Елена <egoncharova72@yandex.ru>
2022-11-23 23:00:47 +0400

    task18

--- task18/Program.cs
+++ task18/Program.cs
@@ -1,18 +1,36 @@
 ﻿// Напишите программу, которая по заданному номеру четверти, показыает диапазон возможных
 // кординат точек в этой четверти (x и y)
-Console.Clear();
-Console.Write("введите номер четверти: ");
-int res= int.Parse(Console.ReadLine());
+// Console.Clear();
+// Console.Write("введите номер четверти: ");
+// int res= int.Parse(Console.ReadLine());
 
-string Quarter( int  result)
+// string Quarter( int  result)
+// {
+//   if (result == 1) return "x>0 и y>0" ; 
+//   if (result == 2 )return "x<0 и y>0 "; 
+//   if (result == 3 )return"x<0 и y<0 "; 
+//   if (result == 4 )return "x>0 и y<0" ; 
+//   return  "ввели неправильное число";
+// }
+
+// string number= Quarter( res);
+// Console.WriteLine(number );
+// Console.WriteLine (Quarter( res));
+
+// решение при помощи строчной (String) переменной
+Console.Write("введите номер четверти: ");
+string res= Console.ReadLine();
+
+string Quarter( string  result)
 {
-  if (result == 1) return "x>0 и y>0" ; 
-  if (result == 2 )return "x<0 и y>0 "; 
-  if (result == 3 )return"x<0 и y<0 "; 
-  if (result == 4 )return "x>0 и y<0" ; 
+  if (result == "1" ) return "x>0 и y>0" ; 
+  if (result == "2" )return "x<0 и y>0 "; 
+  if (result == "3")return"x<0 и y<0 "; 
+  if (result == "4")return "x>0 и y<0" ;
   return  "ввели неправильное число";
 }
 
+// строчный ввод должен быть в <"">
 string number= Quarter( res);
 Console.WriteLine(number );
 Console.WriteLine (Quarter( res));
