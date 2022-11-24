e7e051505dfee95a309f3a2d49c090a282ef77d0
Елена <egoncharova72@yandex.ru>
2022-11-24 22:18:30 +0400

    task20

--- task20/Program.cs
+++ task20/Program.cs
@@ -3,4 +3,33 @@
 
 // А(3,6);В(2,1)->5,09
 // А(7,-5);В(1,-1)->7,21
-Console.WriteLine("Hello, World!");
+
+Console.Clear();
+
+Console.Write("Введите  координаты точки Ax ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите  координаты точки Ay ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите  координаты точки Bx: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите  координаты точки By: ");
+int y2 = int.Parse(Console.ReadLine());
+
+double Distance(int ax, int ay, int bx, int by)
+{
+    return Math.Sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay));
+
+}
+
+double distance =Math.Round (Distance(x1, y1, x2, y2),2,MidpointRounding.ToZero);
+Console.WriteLine(distance);
+
+
+
+
+
+
+// double res = Math.Sqrt(a * b);
+// double res1 = Math.Round(res, 2, MidpointRounding.ToZero);
+// Console.WriteLine(res1);
