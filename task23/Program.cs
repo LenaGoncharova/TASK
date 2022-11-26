a467bdbd7224d1b5e63ab4b75c26f4c3cf8be03f
Елена <egoncharova72@yandex.ru>
2022-11-26 22:31:46 +0400

    23

---
+++ task23/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+Write("Введите целое число:- ");
+int n = Convert.ToInt32(ReadLine());
+
+int CubNun(int a)
+{
+int resalt = Convert.ToInt32(Math.Pow(a,3));
+  return resalt;    
+} 
+int res = CubNun( n);
+if (n>0)
+WriteLine($" {n}->{res,4}");
+else 
+{
+    WriteLine("!");
+}
