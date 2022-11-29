25a6c8d2e472489486156294fd2226c9013c24ec
Елена <egoncharova72@yandex.ru>
2022-11-29 12:15:26 +0400

    14

--- task14/Program.cs
+++ task14/Program.cs
@@ -2,26 +2,40 @@
 // кратно ли оно одновременно 7ии 23.
 Console.Clear();
 
-Console.Write("Введите целое число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-bool MultiplicityTwoDigits (int num)
-{
-return num % 7 == 0 && num % 23 == 0;
-}
-
-bool result = MultiplicityTwoDigits(number);
-Console.WriteLine(result ? "Да" : "Нет");
-
 // Console.Write("Введите целое число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// int number = Convert.ToInt32(Console.ReadLine());
 
-bool MultiplicityTwoDigits (int num, int num1, int num2)
+// bool MultiplicityTwoDigits (int num)
+// {
+// return num % 7 == 0 && num % 23 == 0;
+// }
+
+// bool result = MultiplicityTwoDigits(number);
+// Console.WriteLine(result ? "Да" : "Нет");
+
+// // Console.Write("Введите целое число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// bool MultiplicityTwoDigits (int num, int num1, int num2)
+// {
+// return num % num1 == 0 && num % num2 == 0;
+// }
+
+// bool result = MultiplicityTwoDigits(number, 7, 23);
+
+// Console.Write($"{number} -> ");
+// Console.WriteLine(result ? "Да" : "Нет");
+
+
+
+Console.Write ("введите любое целое число:- ");
+int n=  Math.Abs( Convert.ToInt32(Console.ReadLine()));
+
+bool Multiplicity ( int a,int b ,int c )
 {
-return num % num1 == 0 && num % num2 == 0;
+ return a%b==0 && a % c ==0;
+ 
 }
 
-bool result = MultiplicityTwoDigits(number, 7, 23);
-
-Console.Write($"{number} -> ");
-Console.WriteLine(result ? "Да" : "Нет");
+bool multiplicity = Multiplicity( n,7,23 );
+Console. WriteLine( multiplicity  ? "Да" :"Нет");
