92d7ec5c757ecea875c867657cc90c0fe24afebb
Елена <egoncharova72@yandex.ru>
2022-11-30 21:31:31 +0400

    24

---
+++ task24/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход
+//  число A и выдает сумму чисел от 1 до A; 7->28; 4->10
+Console.Clear();
+Console.WriteLine("введите число ");
+int n =Convert.ToInt32(Console.ReadLine());
+
+ int Summ (int a)
+ {
+    int res=0;     
+   for ( int i=1; i<=a;i++)
+   {
+        res+=i;  //   res=res+i;
+   }
+    return res;
+ }
+  if (n>0)
+  {
+   int summ = Summ (n);
+  Console.WriteLine($"сумма чисел от  1  до {n} = {summ}");
+
+  }
+else
+  Console.WriteLine(" ввели не натуральное число");
