13abe88ee996e5ebb9a64e73b5879e283a484ae7
Елена <egoncharova72@yandex.ru>
2022-11-30 22:08:12 +0400

    26

---
+++ task26/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдает количество цифры в числе :435->3/
+Console.Clear();
+Console.WriteLine("введите целое число ");
+int num =Convert.ToInt32(Console.ReadLine());
+
+int NumberDigital(int x)
+{
+    int res=0;
+    while (x>0)
+    {
+      x = x/10;
+      res = res+1;
+    }
+  return res;
+
+}
+ if(num>0) 
+ {
+    int numberDigital=NumberDigital(num);
+ Console.WriteLine($"количество цифр в числе {num}->{numberDigital}");
+
+ }
+ else
+ Console.WriteLine("вы ввели не натуральное число");
