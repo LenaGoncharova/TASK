04f398969e801fb9d7158f289c4dc8676812bbfd
Елена <egoncharova72@yandex.ru>
2022-11-30 22:28:24 +0400

    26

--- task26/Program.cs
+++ task26/Program.cs
@@ -2,24 +2,21 @@
 // и выдает количество цифры в числе :435->3/
 Console.Clear();
 Console.WriteLine("введите целое число ");
-int num =Convert.ToInt32(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 
 int NumberDigital(int x)
 {
-    int res=0;
-    while (x>0)
+    int res = 0;
+    while (x != 0)                 // while (x!=0)  -означает x не равен 0    
     {
-      x = x/10;
-      res = res+1;
+        x = x / 10;
+        res = res + 1;
     }
-  return res;
+    return res == 0 ? 1 : res;
 
 }
- if(num>0) 
- {
-    int numberDigital=NumberDigital(num);
- Console.WriteLine($"количество цифр в числе {num}->{numberDigital}");
 
- }
- else
- Console.WriteLine("вы ввели не натуральное число");
+int numberDigital = NumberDigital(num);
+Console.WriteLine($"количество цифр в числе {num}->{numberDigital}");
+
+
