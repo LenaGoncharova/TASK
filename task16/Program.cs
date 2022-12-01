5b80fd95658277eba094b20dceb5a74468a24209
Елена <egoncharova72@yandex.ru>
2022-12-01 10:10:42 +0400

    16

--- task16/Program.cs
+++ task16/Program.cs
@@ -1,3 +1,15 @@
 ﻿// напишите программу,которая принимает на вход два числа и проверяет,
 // , является ли одно число квадратом другого
+ using static System. Console;
 Console.Clear();
+  WriteLine(" введие первое число:  ");
+ int num1= int.Parse (Console.ReadLine());
+ WriteLine(" введие второе  число:  ");
+   int num2= int.Parse (Console.ReadLine());
+  bool QvartDigit( int digit1, int digit2 )
+  {
+    return  digit1*digit1 == digit2||  digit2*digit2==digit1;
+
+  }
+ bool resalt= QvartDigit( num1, num2 );
+ WriteLine ( resalt ? "Да" : " Нет");
