5174581d9c99339625bfdf6a2fca610570504b57
Елена <egoncharova72@yandex.ru>
2022-12-03 20:11:49 +0400

    22

--- task22/Program.cs
+++ task22/Program.cs
@@ -9,31 +9,31 @@ void SquareNumber(int x)
     int count = 1;
     while (count <= x)
     {
-        int res = count*count;
-        Console.WriteLine ($"{count} {res}");
+        int res = count * count;
+        Console.WriteLine($"{count} {res}");
         count++;
     }
 
 }
 if (tab > 0)
-SquareNumber(tab);
+    SquareNumber(tab);
 else Console.WriteLine("Число не является натуральным");
 
 
-// Console.WriteLine("Введите натуральное число ->");
-// int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число ->");
+int number = Convert.ToInt32(Console.ReadLine());
 
 
-// void Quadre(int num)
-// {
-// int num1 = 1;
-// while (num1 <= num)
-// {
-// int square = num1 * num1;
-// Console.WriteLine($"{num1,4} {square,4}"); цифра < 4 > -показывает количество отступов для выравния таблиц!!
-// num1++;
-// }
-// }
-// if (number > 0)
-// Quadre(number);
-// else Console.WriteLine("Число не является натуральным");
+void Quadre(int num)
+{
+    int num1 = 1;
+    while (num1 <= num)
+    {
+        int square = num1 * num1;
+        Console.WriteLine($"{num1,4} {square,4}"); цифра < 4 > -показывает количество отступов для выравния таблиц!!
+    num1++;
+    }
+}
+if (number > 0)
+    Quadre(number);
+else Console.WriteLine("Число не является натуральным");
