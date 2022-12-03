39fb16299e310ce77e5fd7405e6f064d6e374502
Елена <egoncharova72@yandex.ru>
2022-12-03 21:39:58 +0400

    27

--- tas/Program.cs
+++ tas/Program.cs
@@ -1,19 +1,76 @@
 ﻿
-Console.WriteLine("Введите натуральное число ->");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите натуральное число ->");
+// int number = Convert.ToInt32(Console.ReadLine());
 
 
-void Quadre(int num)
+// void Quadre(int num)
+// {
+//     int num1 = 1;
+//     while (num1 <= num)
+//     {
+//         int square = num1 * num1;
+//         Console.WriteLine($"{num1,4} {square,4}");
+//         num1++;
+//     }
+// }
+// if (number > 0)
+//     Quadre(number);
+// else Console.Write("Число не является натуральным");
+
+
+// int[] array = new int[8];
+// void FillArray (int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(0, 100);
+//     }
+// }
+
+
+
+int[] array = new int[12];
+void NewArr(int[] arr)
 {
-    int num1 = 1;
-    while (num1 <= num)
+    for (int i = 0; i < arr.Length; i++)
     {
-        int square = num1 * num1;
-        Console.WriteLine($"{num1,4} {square,4}");
-        num1++;
+        arr[i] = new Random().Next(0, 15);
     }
+
 }
-if (number > 0)
-    Quadre(number);
-else Console.Write("Число не является натуральным");
+
+void PrintArr (int[] arr)
+{
+    for(int i=0;i<arr.Length;i++)
+    {
+        Console.Write( $" {arr[i]},");
+
+    }
+
+}
+
+NewArr(array);
+PrintArr(array);
+    
+ int[]inf=new int[12] ; 
+ void FillAr(int[]coll)
+ {
+    for (int i = 0; i <coll.Length; i++)
+
+    {
+        coll[i]=new Random().Next(-7,19);
+        
+    }
+
+ }
+
+ void PrintAr(int[] collect)
+ {
+   for (int i = 0; i <collect. Length; i++)
+   {
+     Console.WriteLine($"{collect[i]} ");
+   }
+}
+ FillAr( inf);
+ PrintAr(inf);
 
